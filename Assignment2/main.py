88db0861de87b2a14e545e6d67145cfce8f90587
farmerp <farmerp@tcd.ie>
2024-10-08 19:10:23 +0100

    Created working base circuit

--- Assignment2/main.py
+++ Assignment2/main.py
@@ -1,18 +1,21 @@
 import qiskit
 import superdenseCoding_classes
 from qiskit_ibm_runtime import QiskitRuntimeService
-from qiskit import QuantumCircuit, transpile, assemble, Aer, AerSimulator, execute
+import os
 
 print(f"Qiskit version: {qiskit.__version__}")
 
-with open('../apiKey.txt', 'r') as file:
+current_dir = os.path.dirname(os.path.abspath(__file__))
+api_key_path = os.path.join(current_dir, '..', 'apiKey.txt')
+
+with open(api_key_path, 'r') as file:
     api_key = file.read().strip()
 
-service = QiskitRuntimeService(channel="ibm_quantum", token=api_key)
+## service = QiskitRuntimeService(channel="ibm_quantum", token=api_key)
+## 
+## backend = service.backend(name="ibm_brisbane")
+## print(f"Number of qubits in backend: {backend.num_qubits}")
 
-backend = service.backend(name="ibm_brisbane")
-print(f"Number of qubits in backend: {backend.num_qubits}")
 
-
-run_quantum_circuit()
+superdenseCoding_classes.run_quantum_circuit()
 
