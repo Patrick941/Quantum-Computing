ec3548ce93f8716f2f0b8b9aec2fb4b22caa6369
farmerp <farmerp@tcd.ie>
2024-10-09 13:46:13 +0100

    Added simple fix for deutsch printing

--- Assignment2/deutsch_algorithm.py
+++ Assignment2/deutsch_algorithm.py
@@ -1,7 +1,7 @@
 import qiskit
 from qiskit import *
 
-def run_quantum_circuit(message_qubit, backend):
+def run_quantum_circuit(backend):
     qc = QuantumCircuit(3, 3)
     
     qc.measure_all()
@@ -11,11 +11,4 @@ def run_quantum_circuit(message_qubit, backend):
     job = backend.run(qc_compiled) 
     result = job.result()
     counts = result.get_counts()
-    alpha0 = round(message_qubit[0] ** 2, 3)
-    alpha1 = round(message_qubit[1] ** 2, 3)
-    print(f"Message Qubits: {alpha0}|0⟩ + {alpha1}|1⟩")
-    print(f"Received Qubits: ", end="")
-    for key, value in counts.items():
-        sum_values = sum(counts.values())
-        print(f"{round(value/sum_values, 3)}|{key[0]}⟩ ", end="")
-    print("")
+    print(counts)
